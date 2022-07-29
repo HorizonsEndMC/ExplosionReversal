6ecb4035fc8c2be6b21d63f53f9ee6b40f64efce
Peter Crawley <peterzcrawley@gmail.com>
2022-07-29 19:13:51 +0100

    Update to 1.19.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,17 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion.of
 
 plugins {
 	java
-	id("io.papermc.paperweight.userdev") version "1.3.7"
+	id("io.papermc.paperweight.userdev") version "1.3.8"
 }
 
 repositories {
-	maven("https://papermc.io/repo/repository/maven-public/") // Paper
-
 	mavenCentral()
+
+	maven("https://papermc.io/repo/repository/maven-public/") // Paper
 }
 
 dependencies {
-	paperDevBundle("1.19-R0.1-SNAPSHOT")
+	paperDevBundle("1.19.1-R0.1-SNAPSHOT")
 }
 
 java.toolchain.languageVersion.set(of(17))
